522ffe7b418891e93f787d9130dd6fa2a0a0d82a
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-29 18:45:58 -0600

    Setup the initial migration.

--- data/migrations/20200429182515_inital.js
+++ data/migrations/20200429182515_inital.js
@@ -1,8 +1,36 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable("zoos", (table) => {
+        table.increments("id")
+        table.text("name").notNullable()
+        table.text("address").notNullable().unique()
+    })
+
+    await knex.schema.createTable("species", (table) => {
+        table.increments("id")
+        table.text("name").notNullable()
+    })
+
+    await knex.schema.createTable("animals", (table) => {
+        table.increments("id")
+        table.text("name").notNullable()
+        table.integer("species_id").references("id").inTable("species")
+    })
+
+    await knex.schema.createTable("zoos_animals", (table) => {
+        table.integer("zoo_id").references("id").inTable("zoos")
+        table.integer("animal_id").references("id").inTable("animals")
+        table.date("from_date").defaultTo(knex.raw("current_timestame"))
+        table.date("to_date")
+        table.primary(["zoo_id", "animal_id"])
+    })
+
+
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("animals")
+    await knex.schema.dropTableIfExists("species")
+    await knex.schema.dropTableIfExists("zoos")
+    await knex.schema.dropTableIfExists("zoos_animals")
 };
